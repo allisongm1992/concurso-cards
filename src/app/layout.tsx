0cfe0b754e28c2140bde1a18c9706a95f6caa471
allis <allis@user.com>
2026-06-29 21:40:38 -0300

    fix: force black bg with !important on html+body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="bg-black text-neutral-100 min-h-screen">
+      <body className="bg-[#000000] text-neutral-100 min-h-screen">
         {children}
         <script
           dangerouslySetInnerHTML={{
